501fae33e748c3b1d35ebfc989297ddd5096e731
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-05-21 13:54:49 -0700

    adjusted home journal posts display thumbnails

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -3,26 +3,31 @@
 get_header(); ?>
 
 <!--Add 3 latest blog posts-->
-<ul class="home-latest-posts">
-  <?php $the_query = new WP_Query( 'posts_per_page=3' ); ?>
-  <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-  <li class="home-display-post"> 
-    <?php if ( has_post_thumbnail() ) : ?>
-      <?php the_post_thumbnail( 'medium' ); ?>
-    <?php endif; ?>
-
-    <?php if ( 'post' === get_post_type() ) : ?>
-      <p><?php the_time('j F Y'); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?></p>
-    <?php endif; ?>
-    
-    <a href="<?php the_permalink() ?>"><h3><?php the_title(); ?></h3></a>
-    <a href="<?php the_permalink() ?>"><p>Read More</p></a>
-  </li>
-  <?php
-    endwhile;
-    wp_reset_postdata();
-  ?>
-</ul>
-
+<div class="home-journal-display">
+  <h2>Inhabitent Journal</h2>
+  <ul class="home-latest-posts">
+    <?php $the_query = new WP_Query( 'posts_per_page=3' ); ?>
+    <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+    <li class="home-display-post"> 
+      <div class="post-thumbnail">
+        <?php if ( has_post_thumbnail() ) : ?>
+          <?php the_post_thumbnail( 'medium' ); ?>
+        <?php endif; ?>
+      </div>
+      <div>
+        <?php if ( 'post' === get_post_type() ) : ?>
+          <p class="post-meta-data"><?php the_time('j F Y'); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?></p>
+        <?php endif; ?>
+        
+        <a href="<?php the_permalink() ?>"><h3><?php the_title(); ?></h3></a>
+        <a href="<?php the_permalink() ?>"><p class="post-read-more">Read More</p></a>
+      </div>
+    </li>
+    <?php
+      endwhile;
+      wp_reset_postdata();
+    ?>
+  </ul>
+</div>
 
 <?php get_footer(); ?>
