9a5df81e8e044ede172ce4d15b0b8fce5bce647b
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-05-23 16:53:41 -0700

    added custom field code for banner image to about.php

--- themes/inhabitent/about.php
+++ themes/inhabitent/about.php
@@ -10,6 +10,26 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		<?php
+			$upload_path = content_url() . '/uploads/';
+				
+			$fields = CFS()->get('banner_image');
+			foreach ($fields as $field) {
+					
+				$banner_image_ID = $field['banner'];
+				$banner_image_ALT = get_post_meta($banner_image_ID, '_wp_attachment_image_alt', true);
+				$banner_image_TITLE = get_the_title($banner_image_ID);
+				$banner_image_URL_data = wp_get_attachment_metadata($banner_image_ID, true);
+				$banner_image_URL = $banner_image_URL_data["file"];
+					
+					echo '<img src="';
+					echo $upload_path . $banner_image_URL;
+					echo '" title="'.$banner_image_TITLE.'" alt="';
+					echo $banner_image_ALT;
+					echo '">';
+			}
+		?>
+
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
