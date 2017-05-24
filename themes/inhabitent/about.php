c9215e73c0546e097366678a1794180692876832
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-05-23 22:19:49 -0700

    added products, styled about.php content

--- themes/inhabitent/about.php
+++ themes/inhabitent/about.php
@@ -10,34 +10,36 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-			$upload_path = content_url() . '/uploads/';
-				
-			$fields = CFS()->get('banner_image');
-			foreach ($fields as $field) {
+			<?php
+				$upload_path = content_url() . '/uploads/';
 					
-				$banner_image_ID = $field['banner'];
-				$banner_image_ALT = get_post_meta($banner_image_ID, '_wp_attachment_image_alt', true);
-				$banner_image_TITLE = get_the_title($banner_image_ID);
-				$banner_image_URL_data = wp_get_attachment_metadata($banner_image_ID, true);
-				$banner_image_URL = $banner_image_URL_data["file"];
-					
-					echo '<img src="';
+				$fields = CFS()->get('banner_image');
+				foreach ($fields as $field) {
+						
+					$banner_image_ID = $field['banner'];
+					// $banner_image_ALT = get_post_meta($banner_image_ID, '_wp_attachment_image_alt', true);
+					// $banner_image_TITLE = get_the_title($banner_image_ID);
+					$banner_image_URL_data = wp_get_attachment_metadata($banner_image_ID, true);
+					$banner_image_URL = $banner_image_URL_data["file"];
+						
+					echo '<div class="about-hero" style="background-image: url(';
 					echo $upload_path . $banner_image_URL;
-					echo '" title="'.$banner_image_TITLE.'" alt="';
-					echo $banner_image_ALT;
-					echo '">';
-			}
-		?>
+					echo ')" ><h1>About</h1></div>';
+				}
+			?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			
+			<div class="about-content">
+				<h2>Our Story</h2>
+				<?php echo CFS()->get( 'our_story' ); ?>
+				<h2>Our Team</h2>				
+				<?php	echo CFS()->get( 'our_team' ); ?>
+			</div>
 
 			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
