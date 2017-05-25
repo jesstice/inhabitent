f5b920ffd7cceb87e2f861da1bf6a98ace09b7ea
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-05-24 21:26:11 -0700

    change journal display on home page to get_posts()

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying archive pages.
+ * The template for displaying product archive pages.
  *
  * @package RED_Starter_Theme
  */
--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -30,32 +30,30 @@ get_header(); ?>
 </div>
 
 <!--Display 3 latest blog posts-->
+
 <div class="home-journal-display">
   <h2>Inhabitent Journal</h2>
+
   <ul class="home-latest-posts">
-    <?php $the_query = new WP_Query( 'posts_per_page=3' ); ?>
-    <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-    <li class="home-display-post"> 
-      <div class="post-thumbnail">
+    <?php
+    $args = array( 'posts_per_page' => 3);
+    $myposts = get_posts( $args );
+    foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+      <li class="home-display-post"> 
         <?php if ( has_post_thumbnail() ) : ?>
           <?php the_post_thumbnail( 'medium' ); ?>
         <?php endif; ?>
-      </div>
-      <div class="post-display-data">
-        <?php if ( 'post' === get_post_type() ) : ?>
-          <p class="post-meta-data"><?php the_time('j F Y'); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?></p>
-        <?php endif; ?>
-        
-        <a href="<?php the_permalink() ?>"><h3><?php the_title(); ?></h3></a>
-        <a class="post-read-more" href="<?php the_permalink() ?>">Read Entry</a>
-      </div>
-    </li>
-    <?php
-      endwhile;
-      wp_reset_postdata();
-    ?>
+        <div class="post-display-data">
+          <?php if ( 'post' === get_post_type() ) : ?>
+            <p class="post-meta-data"><?php the_time('j F Y'); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?></p>
+          <?php endif; ?>
+          <a href="<?php the_permalink() ?>"><h3><?php the_title(); ?></h3></a>
+          <a class="post-read-more" href="<?php the_permalink() ?>">Read Entry</a>
+        </div>
+      </li>
+    <?php endforeach; 
+    wp_reset_postdata();?>
   </ul>
-</div>
 
 <!--Adventures Section-->
 <div>
