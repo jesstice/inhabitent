49f792cc36f0ef30e15c0fc37f6d23f826a56344
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-05-27 14:13:36 -0700

    add spacing to footer, size icons

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -11,25 +11,26 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="container">
+				
 				<div id="footer-sidebar" class="secondary">
-						<?php
-						if(is_active_sidebar('footer-1')){
+					<?php if(is_active_sidebar('footer-1')){
 							dynamic_sidebar('footer-1');
 						}
-						?>
+					?>
 				</div>
 
-					<div class="footer-logo">
-						<a href="<?php echo get_home_url(); ?>">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/inhabitent-logo-text.svg" alt="inhabitent logo"> 
-						</a>
-					</div>
-
-				</div><!-- .site-info -->
+				<div class="footer-logo">
+					<a href="<?php echo get_home_url(); ?>">
+						<img src="<?php echo get_stylesheet_directory_uri() ?>/images/inhabitent-logo-text.svg" alt="inhabitent logo"> 
+					</a>
+				</div>
 
 				<div class="footer-copyright">
 					<p>Copyright &copy; 2016 Inhabitent</p>
 				</div>
+
+				</div><!-- .site-info -->
+				
 			</div>
 		</footer><!-- #colophon -->
 		
